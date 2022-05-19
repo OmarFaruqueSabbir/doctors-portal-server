a1a98d42337a0b28e477db6cc2dcee965359a8f4
OmarFaruqueSabbir <avrosabbir18@gmail.com>
2022-05-19 19:53:21 +0600

    payment confirmation email outline created

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ async function run() {
         const bookingCollection = client.db('doctors_portal').collection('bookings')
         const userCollection = client.db('doctors_portal').collection('users')
         const doctorCollection = client.db('doctors_portal').collection('doctors')
+        const paymentCollection = client.db('doctors_portal').collection('payments')
 
         const verifyAdmin = async (req, res, next) => {
             const requester = req.decoded.email;
@@ -215,6 +216,22 @@ async function run() {
             const result = bookingCollection.insertOne(booking);
             res.send({ success: true, result });
         });
+
+        app.patch('/bookings/:id', verifyJWT, async(req, res) =>{
+          const id  = req.params.id;
+          const payment = req.body;
+          const filter = {_id: ObjectId(id)};
+          const updatedDoc = {
+            $set: {
+              paid: true,
+              transactionId: payment.transactionId
+            }
+          }
+    
+          const result = await paymentCollection.insertOne(payment);
+          const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
+          res.send(updatedBooking);
+        })
 
         //get all doctors
         app.get('/doctor', verifyJWT, verifyAdmin, async(req, res) =>{
